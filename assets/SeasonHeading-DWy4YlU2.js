9ca59229b9595a3d6a7335f46bd7c8704aea7eda
Yuriy Victor <dread.naut@ymail.com>
2025-05-11 19:52:39 +0200

    WIP 20250511 OSM & Leaflet Improved

--- assets/SeasonHeading-DWy4YlU2.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as n,a as o,v as t,t as r,g as c}from"./index-BvpA_yDi.js";const d={name:"SeasonHeading",props:["season"]};function p(i,a,e,l,_,f){return c(),n("h2",null,[a[0]||(a[0]=o("span",{class:"visually-hidden"},"Season: ",-1)),t(r(e.season),1)])}const u=s(d,[["render",p],["__scopeId","data-v-da247b5a"]]);export{u as S};
